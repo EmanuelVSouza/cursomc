dd74cb1cea64701cfb9223e8bad4aaf610a67316
EmanuelVSouza <emanuel_vixboy@hotmail.com>
2022-03-08 02:47:03 -0300

    Criando repository e service para Categoria

---
+++ src/main/java/com/devnel/cursomc/repositories/CategoriaRepository.java
@@ -0,0 +1,10 @@
+package com.devnel.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.devnel.cursomc.domain.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
+}
