b9a25967e523f77964146ecfa5a4e00768e2681a
EmanuelVSouza <emanuel_vixboy@hotmail.com>
2022-03-10 13:33:35 -0300

    Estado e Cidade

---
+++ src/main/java/com/devnel/cursomc/repositories/EstadoRepository.java
@@ -0,0 +1,10 @@
+package com.devnel.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.devnel.cursomc.domain.Estado;
+
+@Repository
+public interface EstadoRepository extends JpaRepository<Estado, Integer> {
+}
